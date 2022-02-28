d34b0d65d10a1ebf0ac8fc1e13e943eee3b4aa06
cmmichaelwork <82690535+cmmichaelwork@users.noreply.github.com>
2022-02-28 12:44:18 -0500

    Add files via upload

--- AzureHttpFunctionArguments.cs
+++ AzureHttpFunctionArguments.cs
@@ -19,3 +19,28 @@ public static async Task<IActionResult> Run(
         ? (ActionResult)new OkObjectResult($"Hello, {name}")
         : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
 }
+
+[FunctionName("HttpTriggerCSharp")]
+public static Task<IActionResult> Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req)
+{
+    ...
+}
+
+public static class EnvironmentVariablesExample
+{
+    [FunctionName("GetEnvironmentVariables")]
+    public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        log.LogInformation(GetEnvironmentVariable("AzureWebJobsStorage"));
+        log.LogInformation(GetEnvironmentVariable("FireTemple"));
+        log.LogInformation(GetEnvironmentVariable("Admin@defcorpspace.onmicrosoft.com")
+    }
+
+    private static string GetEnvironmentVariable(string name)
+    {
+        return name + ": " +
+            System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+    }
+}
